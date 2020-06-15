00b49095f4ea9917c4bf2112fbb2dd0262f9a214
RickyC0626 <rickychon99@gmail.com>
2020-06-15 14:32:23 -0400

    Add sidemenu view template

---
+++ src/view/SideMenu/SideMenuView.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SideMenuView = (props) =>
+{
+    return (
+        <div>
+            <p>Side Menu</p>
+        </div>
+    );
+}
+
+export default SideMenuView;
