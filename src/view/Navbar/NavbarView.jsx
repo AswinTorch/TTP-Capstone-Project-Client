d1e33f733251f2cc772cff372181e1c036d86d61
Aswin Nair <aswintorch@gmail.com>
2020-06-15 14:50:40 -0400

    Styled navbar component with dynamic props from firebase user info

--- src/view/Navbar/NavbarView.jsx
+++ src/view/Navbar/NavbarView.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-const NavbarView = () => {
-  return <div></div>;
+const NavbarView = (props) => {
+  return (
+    <nav className="navbar navbar-light bg-dark">
+      <span className="navbar-brand mb-0 h1 text-white">
+        STUDENT<span style={{ color: "orange" }}>First</span>
+      </span>
+      <div className="form-inline text-white">
+        <span className="mr-3">{props.displayName}</span>
+        <img
+          src={props.photoURL}
+          alt=""
+          className="rounded-circle"
+          style={{ height: 40, width: 40 }}
+        />
+      </div>
+    </nav>
+  );
+};
+
+NavbarView.defaultProps = {
+  displayName: null,
+  photoURL: null,
 };
 
 export default NavbarView;
