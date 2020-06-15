20a93221a9427cb1399ce86ec8ffa4df7a6b1759
RickyC0626 <rickychon99@gmail.com>
2020-06-15 14:35:51 -0400

    Add sidemenu view to dashboard view

--- src/view/Dashboard/DashboardView.jsx
+++ src/view/Dashboard/DashboardView.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import firebase from "../../firebase";
 
+import SideMenuView from "../SideMenu/SideMenuView";
+
 const DashboardView = (props) => {
   return (
     <div className="container">
       <h1>Welcome, {firebase.auth().currentUser.displayName}</h1>
+      <img src={firebase.auth().currentUser.photoURL} width="150px" alt="user" />
+      <br/>
       <button onClick={() => firebase.auth().signOut()}>Sign Out</button>
-      <img src={firebase.auth().currentUser.photoURL} alt="user" />
+
+      <SideMenuView />
     </div>
   );
 };
