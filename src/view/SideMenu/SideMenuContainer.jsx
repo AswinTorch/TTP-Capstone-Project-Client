90ff4875d4b73ca394c51e2f77ae04ae2087c68b
RickyC0626 <rickychon99@gmail.com>
2020-06-15 15:05:34 -0400

    Add sidemenu container

---
+++ src/view/SideMenu/SideMenuContainer.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import SideMenuView from "./SideMenuView";
+
+class SideMenuContainer extends Component
+{
+    render()
+    {
+        return <SideMenuView />;
+    }
+}
+
+export default SideMenuContainer;
