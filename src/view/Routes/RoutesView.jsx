f31434108a4bd96fdf83f3058bfe5d208be9286d
RickyC0626 <rickychon99@gmail.com>
2020-06-15 16:29:02 -0400

    Add finance container and view, along with route

--- src/view/Routes/RoutesView.jsx
+++ src/view/Routes/RoutesView.jsx
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 
 import DashboardContainer from "../Dashboard/DashboardContainer";
 import EnrollmentContainer from "../Enrollment/EnrollmentContainer";
+import FinanceContainer from "../Finance/FinanceContainer";
 
 const RoutesView = () =>
 {
@@ -11,6 +12,7 @@ const RoutesView = () =>
             <Route exact path="/" component={DashboardContainer} />
             <Route exact path="/dashboard" component={DashboardContainer} />
             <Route exact path="/enrollment" component={EnrollmentContainer} />
+            <Route exact path="/finances" component={FinanceContainer} />
         </Switch>
     );
 }
