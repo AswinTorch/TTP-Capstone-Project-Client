775ce51768dc58a758fa1213d9a46b68d05206e1
Aswin Nair <aswintorch@gmail.com>
2020-06-16 11:52:39 -0400

    Added reducer for fetching all courses

--- src/state/enrollment/reducers.js
+++ src/state/enrollment/reducers.js
@@ -1,15 +1,15 @@
 import types from "./action_types";
 
 // Reducer for enrollment
-const reducer = (state = [], action) =>
-{
-    switch(action.type)
-    {
-        case types.FETCH_REGISTERED_COURSES:
-            return action.payload;
-        default:
-            return state;
-    }
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case types.FETCH_REGISTERED_COURSES:
+      return action.payload;
+    case types.FETCH_ALL_COURSES:
+      return action.payload;
+    default:
+      return state;
+  }
 };
 
 export default reducer;
