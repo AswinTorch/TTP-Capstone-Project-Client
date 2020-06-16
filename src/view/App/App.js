53ea6bb06e0e99de05df95ba2ef76279a1908052
RickyC0626 <rickychon99@gmail.com>
2020-06-16 19:59:25 -0400

    Add implementation for user registration to the database

--- src/view/App/App.js
+++ src/view/App/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
-import firebase from "../../firebase";
+
+import firebase from "firebase";
+import axios from "axios";
+
 import LoginView from "../Login/LoginView";
 import NavbarView from "../Navbar/NavbarView";
 import RoutesContainer from "../Routes/RoutesContainer";
 import SideMenuContainer from "../SideMenu/SideMenuContainer";
+
 
 class App extends Component 
 {
@@ -24,7 +28,8 @@ class App extends Component
     };
 
     /**
-     * Listen for authentication on mount
+     * Listen for authentication on mount, then send a POST request to register the new user, 
+     * or connect to an existing one
      */
     componentDidMount() 
     {
@@ -34,12 +39,37 @@ class App extends Component
     /**
      * Check if user has been authenticated, then set their signed in state to correspond with the 
      * authentication
+     * 
+     * Afterwards, send a POST request to the backend to register the new user or connect to an existing one
      */
     authListener() 
     {
         firebase.auth().onAuthStateChanged((user) => 
         {
             this.setState({ isSignedIn: !!user });
+            
+            if(user)
+            {
+                const uid = user.uid;
+                
+                // Let's send a request to check if user exists
+                axios.get(`/api/students/id/${uid}`)
+                .then((res) => res.data)
+                .catch((err) =>
+                {
+                    // User doesn't exist on database, let's make a new one
+                    const displayName = user.displayName.split(" ");
+                    const firstName = displayName[0];
+                    const lastName = displayName[displayName.length - 1];
+                    const email = user.email;
+
+                    let student = { uid, firstName, lastName, email };
+
+                    axios.post("/api/students/", student)
+                    .then((res) => res.data)
+                    .catch((err) => console.error(err));
+                });
+            }
         });
     }
 
