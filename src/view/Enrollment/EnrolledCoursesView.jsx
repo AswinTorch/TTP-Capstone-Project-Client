0e98aa1f75bcfc9bc4bbb739624bbf0c752813fc
RickyC0626 <rickychon99@gmail.com>
2020-06-16 00:33:08 -0400

    Add courses to enrolled courses

--- src/view/Enrollment/EnrolledCoursesView.jsx
+++ src/view/Enrollment/EnrolledCoursesView.jsx
@@ -6,7 +6,20 @@ const EnrolledCoursesView = (props) =>
         <div className="card mt-4 shadow border-0">
             <h5 className="card-header text-primary">Your Courses</h5>
             <div className="card-body">
-                You are not enrolled in any courses at this time.
+                <ul className="list-group">
+                    <li className="list-group-item list-group-item-action btn btn-outline-warning justify-content-between align-items-center d-flex">
+                        CISC. 3440: Machine Learning
+                        <span className="badge badge-success">3 credits</span>
+                    </li>
+                    <li className="list-group-item list-group-item-action btn btn-outline-warning justify-content-between align-items-center d-flex">
+                        CISC. 3660: Game Programming
+                        <span className="badge badge-success">3 credits</span>
+                    </li>
+                    <li className="list-group-item list-group-item-action btn btn-outline-warning justify-content-between align-items-center d-flex">
+                        CISC. 3665: Game Design
+                        <span className="badge badge-success">3 credits</span>
+                    </li>
+                </ul>
             </div>
         </div>
     );
