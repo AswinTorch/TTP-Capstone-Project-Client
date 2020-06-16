ae9ea37fd28cf290eecc8faa1bcf919bf644276d
RickyC0626 <rickychon99@gmail.com>
2020-06-16 14:51:38 -0400

    Add documentation for main App methods

--- src/view/App/App.js
+++ src/view/App/App.js
@@ -6,59 +6,72 @@ import NavbarView from "../Navbar/NavbarView";
 import RoutesContainer from "../Routes/RoutesContainer";
 import SideMenuContainer from "../SideMenu/SideMenuContainer";
 
-class App extends Component {
-  state = {
-    isSignedIn: false,
-  };
+class App extends Component 
+{
+    state = { isSignedIn: false };
 
-  // Firebase auth config object
-  uiConfig = {
-    signInFlow: "popup",
-    signInOptions: [
-      // List of OAuth providers supported.
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    ],
-    callbacks: {
-      signInSuccessWithAuthResult: () => false,
-    },
-  };
+    uiConfig = 
+    {
+        // Shows pop up window for signing in
+        signInFlow: "popup",
+        signInOptions: [
+            // List of OAuth providers supported.
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        ],
+        callbacks: {
+            signInSuccessWithAuthResult: () => false,
+        },
+    };
 
-  // Function called on first render
-  componentDidMount() {
-    this.authListener();
-  }
+    /**
+     * Listen for authentication on mount
+     */
+    componentDidMount() 
+    {
+        this.authListener();
+    }
 
-  // Function to check firebase authentication state -> sets local state of isSignedIn
-  authListener() {
-    firebase.auth().onAuthStateChanged((user) => {
-      this.setState({ isSignedIn: !!user });
-    });
-  }
+    /**
+     * Check if user has been authenticated, then set their signed in state to correspond with the 
+     * authentication
+     */
+    authListener() 
+    {
+        firebase.auth().onAuthStateChanged((user) => 
+        {
+            this.setState({ isSignedIn: !!user });
+        });
+    }
 
-  render() {
-    return (
-      <div className="" style={{ backgroundColor: "#F8F9FC" }}>
-        {this.state.isSignedIn ? (
-          <>
-            <div className="row">
-              <SideMenuContainer />
-              <div className="container-fluid col-10 pl-0">
-                <NavbarView
-                  displayName={firebase.auth().currentUser.displayName}
-                  photoURL={firebase.auth().currentUser.photoURL}
-                />
-                <RoutesContainer />
-              </div>
+    render() 
+    {
+        /**
+         * Display the dashboard only if user is signed in, otherwise 
+         * display the login page
+         */
+        return (
+            <div className="" style={{ backgroundColor: "#F8F9FC" }}>
+                {this.state.isSignedIn ? (
+                    <>
+                        <div className="row">
+                            <SideMenuContainer />
+                            <div className="container-fluid col-10 pl-0">
+                                <NavbarView
+                                    displayName={firebase.auth().currentUser.displayName}
+                                    photoURL={firebase.auth().currentUser.photoURL}
+                                />
+                                <RoutesContainer />
+                            </div>
+                        </div>
+                    </>
+                    ) : (
+                    <>
+                        <LoginView uiConfig={this.uiConfig} />
+                    </>
+                )}
             </div>
-          </>
-        ) : (
-          <>
-            <LoginView uiConfig={this.uiConfig} />
-          </>
-        )}
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default App;
