f08c31527d1f986edc3d1c740986861409ca878f
Aswin Nair <aswintorch@gmail.com>
2020-06-16 11:52:05 -0400

    Added fetchallcourses thunk, action, and action type

--- src/state/enrollment/thunks.js
+++ src/state/enrollment/thunks.js
@@ -1,15 +1,20 @@
 import axios from "axios";
 
-import
-{
-    fetchRegisteredCourses,
-} from "./actions";
+import { fetchRegisteredCourses, fetchAllCourses } from "./actions";
 
 // Thunk creators for enrollment
-export const fetchRegisteredCoursesThunk = (studentId) => (dispatch) =>
-{
-    return axios.get(`/api/students/${studentId}/courses`)
+export const fetchRegisteredCoursesThunk = (studentId) => (dispatch) => {
+  return axios
+    .get(`/api/students/${studentId}/courses`)
     .then((res) => res.data)
     .then((courses) => dispatch(fetchRegisteredCourses(courses)))
+    .catch((err) => console.error(err));
+};
+
+export const fetchAllCoursesThunk = () => (dispatch) => {
+  return axios
+    .get("/api/courses")
+    .then((res) => res.data)
+    .then((courses) => dispatch(fetchAllCourses(courses)))
     .catch((err) => console.error(err));
 };
