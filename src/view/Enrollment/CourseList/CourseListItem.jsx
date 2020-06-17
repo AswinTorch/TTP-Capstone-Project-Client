ce57c7949b9f49ac3128d9b20b6d78761cf8e6da
Aswin Nair <aswintorch@gmail.com>
2020-06-17 13:14:00 -0400

    Added enroll button to CourseListItem component

--- src/view/Enrollment/CourseList/CourseListItem.jsx
+++ src/view/Enrollment/CourseList/CourseListItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Accordion from "react-bootstrap/Accordion";
 import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 /**
  * Represents a single list item in CourseListView
@@ -13,8 +14,10 @@ const CourseListItem = ({ course, index }) => {
     <Card>
       <Accordion.Toggle as={Card.Body} variant="link" eventKey={index}>
         <div className="d-flex justify-content-between align-items-center">
-          {course.course_identifier} {course.course_number}:{" "}
-          {course.course_name}
+          <span className="">
+            {course.course_identifier} {course.course_number}:{" "}
+            {course.course_name}
+          </span>
           <span
             className="badge badge-success d-flex align-items-center"
             style={{ maxHeight: 20 }}
@@ -25,7 +28,10 @@ const CourseListItem = ({ course, index }) => {
       </Accordion.Toggle>
 
       <Accordion.Collapse eventKey={index}>
-        <Card.Body>{course.description}</Card.Body>
+        <Card.Body className="border border-bottom-0 border-left-0 border-right-0">
+          <p>{course.description}</p>
+          <Button variant="outline-success">Enroll</Button>
+        </Card.Body>
       </Accordion.Collapse>
     </Card>
   );
