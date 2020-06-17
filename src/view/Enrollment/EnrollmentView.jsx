13c8adb3ffcda22704f3294ac630f8be380db813
Aswin Nair <aswintorch@gmail.com>
2020-06-17 11:31:24 -0400

    Changed header styling of all components to be less intrusive

--- src/view/Enrollment/EnrollmentView.jsx
+++ src/view/Enrollment/EnrollmentView.jsx
@@ -3,18 +3,17 @@ import React from "react";
 import EnrolledCoursesContainer from "./EnrolledCourses/EnrolledCoursesContainer";
 import CourseListContainer from "./CourseList/CourseListContainer";
 
-const EnrollmentView = (props) => 
-{
-    return (
-        <div className="container-fluid pt-3 pl-4 pr-4 pb-4">
-            <div>
-                <h1>Enrollment</h1>
-            </div>
+const EnrollmentView = (props) => {
+  return (
+    <div className="container-fluid pt-3 pl-4 pr-4 pb-4">
+      <div>
+        <h2 className="text-muted">Enrollment</h2>
+      </div>
 
-                <EnrolledCoursesContainer />
-                <CourseListContainer />
-        </div>
-    );
-}
+      <EnrolledCoursesContainer />
+      <CourseListContainer />
+    </div>
+  );
+};
 
 export default EnrollmentView;
