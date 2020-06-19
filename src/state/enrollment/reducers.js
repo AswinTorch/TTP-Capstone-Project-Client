b3a5fdea2a87def4aa9711518e7afeb06ca8876e
Aswin Nair <aswintorch@gmail.com>
2020-06-19 10:41:39 -0400

    Added reducer for professor reviews

--- src/state/enrollment/reducers.js
+++ src/state/enrollment/reducers.js
@@ -5,14 +5,20 @@ import _ from "lodash";
 const initialState = {
   student: {},
   allCourses: [],
+  professorReviews: [],
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    // Fetches
     case types.FETCH_ALL_COURSES:
       return { ...state, allCourses: action.payload };
     case types.FETCH_STUDENT:
       return { ...state, student: action.payload };
+    case types.FETCH_PROFESSOR_REVIEWS:
+      return { ...state, professorReviews: action.payload };
+
+    // Operations
     case types.ADD_COURSE:
       return {
         ...state,
@@ -26,8 +32,9 @@ const reducer = (state = initialState, action) => {
       const updatedCourses = enrolledCourses.filter(
         (course) => !_.isEqual(course, action.payload)
       );
-
       return { ...state, student: { enrolled_courses: updatedCourses } };
+    // SWAP GOES HERE
+
     default:
       return state;
   }
