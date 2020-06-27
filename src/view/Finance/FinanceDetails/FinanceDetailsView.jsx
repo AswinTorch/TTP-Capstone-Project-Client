d4e9acbc4e31e664917c1c187fbea42b1b248ef6
Aswin Nair <aswintorch@gmail.com>
2020-06-26 20:32:25 -0400

    Refactored FinancialDetailsContainer to functional component

--- src/view/Finance/FinanceDetails/FinanceDetailsView.jsx
+++ src/view/Finance/FinanceDetails/FinanceDetailsView.jsx
@@ -5,44 +5,43 @@ import FinanceDetailsItem from "./FinanceDetailsItem";
 /**
  * Represents the entire view of the user's transaction history
  * This component controls the type of display to use
- * 
+ *
  * Receives props (fetched data) from FinanceDetailsContainer
  * Passes props (fetched data) down to FinanceDetailsItem to generate
  */
-const FinanceDetailsView = (props) =>
-{
-    return (
-        <div className="card mt-4 shadow rounded border-0">
-            <h5 className="card-header text-primary">Transaction History</h5>
-            <Table striped bordered hover responsive trClassName="py-4">
-                <thead>
-                    <tr>
-                        <th>Course</th>
-                        <th>Action</th>
-                        <th>Date</th>
-                        <th>Transaction Amount</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {props.student.transaction_history &&
-                    props.student.transaction_history.length !== 0 ? (
-                    // Shows fetched data if it exists
+const FinanceDetailsView = (props) => {
+  return (
+    <div className="card mt-4 shadow rounded border-0">
+      <h5 className="card-header text-primary">Transaction History</h5>
+      <Table striped bordered hover responsive className="py-4">
+        <thead>
+          <tr>
+            <th>Course</th>
+            <th>Action</th>
+            <th>Date</th>
+            <th>Transaction Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.student.transaction_history &&
+          props.student.transaction_history.length !== 0 ? (
+            // Shows fetched data if it exists
 
-                    props.student.transaction_history.map((transaction) => (
-                        <FinanceDetailsItem
-                            transaction={transaction}
-                            key={transaction.date}
-                        />
-                    ))
-                    ) : (
-                        <tr className="pl-4 pt-3 pb-2">
-                            <td colSpan="4">No transaction history.</td>
-                        </tr>
-                    )}
-                </tbody>
-            </Table>
-        </div>
-    );
+            props.student.transaction_history.map((transaction) => (
+              <FinanceDetailsItem
+                transaction={transaction}
+                key={transaction.date}
+              />
+            ))
+          ) : (
+            <tr className="pl-4 pt-3 pb-2">
+              <td colSpan="4">No transaction history.</td>
+            </tr>
+          )}
+        </tbody>
+      </Table>
+    </div>
+  );
 };
 
 export default FinanceDetailsView;
