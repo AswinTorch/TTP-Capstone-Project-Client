79099519886fdc4900096ad305ae52240102a3a0
RickyC0626 <rickychon99@gmail.com>
2020-06-27 01:24:25 -0400

    Move MoneyOwedView to MoneyOwed, and delete the file

--- src/view/Finance/FinanceOverview/MoneyOwed.jsx
+++ src/view/Finance/FinanceOverview/MoneyOwed.jsx
@@ -3,7 +3,6 @@ import React, { useEffect } from "react";
 import { fetchStudentThunk } from "../../../state/enrollment/thunks";
 import firebase from "../../../firebase";
 
-import MoneyOwedView from "./MoneyOwedView";
 import { useDispatch, useSelector } from "react-redux";
 
 /**
@@ -36,6 +35,31 @@ const MoneyOwed = (props) => {
 
   return <MoneyOwedView moneyOwed={calculateMoneyOwed(student)} />;
 };
+
+const MoneyOwedView = ({ moneyOwed }) =>
+{
+  return (
+    <div className="col-md-3 col-sm-6">
+      <div className="card shadow border-danger border-3 border-bottom-0 border-top-0 border-right-0 pt-2 pb-2 mb-3">
+        <div className="card-body">
+          <div className="row no-gutters align-items-center">
+            <div className="col mr-2">
+              <div className="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                Money Owed
+              </div>
+              <h6 className="card-subtitle mt-3">
+                ${Number(moneyOwed).toFixed(2)}
+              </h6>
+            </div>
+            <div className="col-auto">
+              <i className="fas fa-dollar-sign fa-2x text-light"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 // Export our store-connected container by default
 export default MoneyOwed;
