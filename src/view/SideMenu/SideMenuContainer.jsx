b6177077e900d87e9fd2d40b3d20df0bf2b91f5e
RickyC0626 <rickychon99@gmail.com>
2020-06-27 00:33:19 -0400

    Shorten side menu view file name

--- src/view/SideMenu/SideMenuContainer.jsx
+++ src/view/SideMenu/SideMenuContainer.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import SideMenuView from "./SideMenuView";
+import SideMenu from "./SideMenu";
 
 const SideMenuContainer = () => {
-  return <SideMenuView />;
+  return <SideMenu />;
 };
 
 export default SideMenuContainer;
