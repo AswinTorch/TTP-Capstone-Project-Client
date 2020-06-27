a5c37ac439fffe7f981a65f66e6aa9ae46ad5613
RickyC0626 <rickychon99@gmail.com>
2020-06-27 00:55:09 -0400

    Remove dashboard container, reroute to dashboard component

--- src/view/Dashboard/DashboardContainer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Dashboard from "./Dashboard";
-
-const DashboardContainer = () => {
-  return <Dashboard />;
-};
-
-export default DashboardContainer;
--- src/view/Routes/RoutesView.jsx
+++ src/view/Routes/RoutesView.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import DashboardContainer from "../Dashboard/DashboardContainer";
+import Dashboard from "../Dashboard/Dashboard";
 import EnrollmentContainer from "../Enrollment/EnrollmentContainer";
 import Finance from "../Finance/Finance";
 
 const RoutesView = () => {
   return (
     <Switch>
-      <Route exact path="/" component={DashboardContainer} />
-      <Route exact path="/dashboard" component={DashboardContainer} />
+      <Route exact path="/" component={Dashboard} />
+      <Route exact path="/dashboard" component={Dashboard} />
       <Route exact path="/enrollment" component={EnrollmentContainer} />
       <Route exact path="/finances" component={Finance} />
     </Switch>
